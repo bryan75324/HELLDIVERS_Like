7619a5aed85c03f7ea89439cbd9e535fec8e7e08
BryanHsiao <37688239+bryan75324@users.noreply.github.com>
2018-12-01 16:34:55 +0800

    Add UI image alpha tweener.

---
+++ HellDivers_UnityProject/Assets/Scripts/UI/UITweenImageAlpha.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HELLDIVERS.UI
+{
+    public class UITweenImageAlpha : MonoBehaviour
+    {
+        public float TimeLenght
+        {
+            get
+            { return m_Time; }
+            set
+            {
+                if (value < 0) m_Time = 0;
+                else m_Time = value;
+            }
+        }
+
+        [SerializeField] private Image m_Image;
+        [SerializeField] private float m_Time;
+        [SerializeField] private float m_StartAlpha;
+        [SerializeField] private float m_EndAlpha;
+
+        #region Events
+
+        /// <summary>
+        /// Event on tween animation finished;
+        /// </summary>
+        public event UIEventHolder OnTweenFinished;
+
+        /// <summary>
+        /// Event on tween animation start.
+        /// </summary>
+        public event UIEventHolder OnTweenStart;
+
+        #endregion Events
+    }
+}
